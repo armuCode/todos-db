99af835f7d99e43e505ccb32c5bbf3bacf1631bd
armucode <armucode@gmail.com>
2024-04-19 16:18:55 -0500

    Se crean las funciones del route para validar los objetos por el id del route

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -1,5 +1,8 @@
 import prisma from "@/lib/prisma";
+import { Todo } from "@prisma/client";
+import { get } from "http";
 import { NextResponse, NextRequest } from "next/server";
+import { boolean, object, string } from "yup";
 
 interface Segments {
   params: {
@@ -7,9 +10,33 @@ interface Segments {
   };
 }
 
+const getTodo = async (id: string): Promise<Todo | null> => {
+  const todo = await prisma.todo.findFirst({ where: { id } });
+  return todo;
+};
+
 export async function GET(request: Request, { params }: Segments) {
   const { id } = params;
-  const todo = await prisma.todo.findFirst({ where: { id } });
+  const todo = await getTodo(id);
+  if (!todo) {
+    return NextResponse.json(
+      {
+        message: `Todo con id ${params.id} no existe`,
+      },
+      { status: 404 }
+    );
+  }
+  return NextResponse.json(params.id);
+}
+
+const putSchema = object({
+  complete: boolean().optional(),
+  description: string().optional(),
+});
+
+export async function PUT(request: Request, { params }: Segments) {
+  const { id } = params;
+  const todo = await getTodo(id);
   if (!todo) {
     return NextResponse.json(
       {
@@ -18,5 +45,14 @@ export async function GET(request: Request, { params }: Segments) {
       { status: 404 }
     );
   }
-  return NextResponse.json(params.id);
+
+  const { complete, description } = await putSchema.validate(
+    await request.json()
+  );
+
+  const updateTodo = await prisma.todo.update({
+    where: { id: params.id },
+    data: { complete, description },
+  });
+  return NextResponse.json(updateTodo);
 }
